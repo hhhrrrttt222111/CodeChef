0466fe527f52cd102296ec3a429a880700eea63a
Satyam Chaudhari <sschaudhari98@gmail.com>
2020-10-15 02:14:55 +0530

    Covid Run

--- Beginner/Covid Run/covid_run.cpp
+++ Beginner/Covid Run/covid_run.cpp
@@ -3,11 +3,6 @@ using namespace std;
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
-
     int T;
     cin>>T;
 
