02268287fa6a5bc31593e7d47ee44cc7139f7f18
Hemanth R <hemanthrt18@gmail.com>
2020-10-16 21:58:16 +0530

    👾

---
+++ Beginner/Sticks (STICKS)/sticks.py
@@ -0,0 +1,28 @@
+t = int(input())
+
+while t:
+    N = int(input())
+    L = list(map(int,input().split()))
+    D = dict()
+    for i in range(N):
+        if L[i] not in D:
+            D[L[i]]=1
+        else:
+            D[L[i]]+=1
+    arr = []
+    c = 0
+    for key,value in D.items():
+        if value>1:
+            c=c+1
+            arr.append(key)
+    if True:
+        arr.sort(reverse=True)
+        if D[arr[0]]>3:
+            print(arr[0]*arr[0])
+        elif len(arr)<2:
+            print("-1")
+        else:
+            print(arr[0]*arr[1])
+
+    t = t-1
+        
