d43b8095e650138efe7dd1828fbd3db58d438e99
Hemanth R <hemanthrt18@gmail.com>
2020-10-18 09:26:27 +0530

    👾

--- pretty.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for i in range(t):
-    l, r = map(int, input().split())
-    prettynum = 0 
-    for num in range(l, r + 1):
-        num = str(num)
-        if num[-1] == '2' or num[-1] == '3' or num[-1] == '9':
-            prettynum += 1
-    print(prettynum)
