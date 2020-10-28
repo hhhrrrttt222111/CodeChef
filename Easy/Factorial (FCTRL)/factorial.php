b89d67a5e7a5811251b34857fdd9318fc6214ea2
Kaushik Sriram <kaushik162000@gmail.com>
2020-10-29 05:24:57 +0530

    Changed the wrong solution

--- Easy/Factorial (FCTRL)/factorial.php
+++ Easy/Factorial (FCTRL)/factorial.php
@@ -1,9 +1,21 @@
-<?php  
-    $num = readline();  
-    $factorial = 1;  
-    for ($i=$num; $i>=1; $i--)   
-    {  
-      $factorial = $factorial * $i;  
-    }  
-    echo "$factorial";  
-?>  
+<?php
+
+$test = fgets(STDIN);
+
+for ($i=0; $i < $test; $i++) {
+    $input = fgets(STDIN);
+    echo fact($input)."\r\n";
+}
+
+function fact($input)
+{
+    $fact=5;
+    $count=0;
+    $input = (int)$input;
+    while ($fact<=$input) {
+        $count = (int)($count + $input / $fact);
+        $fact = $fact*5;
+    }
+
+    return $count;
+}
