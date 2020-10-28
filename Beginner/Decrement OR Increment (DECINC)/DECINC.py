0dd820eab790960731003d8ea74d1738140cb6f9
Tom <58892488+toms0@users.noreply.github.com>
2020-10-28 23:32:56 +0000

    Added input validation + functional structure to 'DECINC.py'

--- Beginner/Decrement OR Increment (DECINC)/DECINC.py
+++ Beginner/Decrement OR Increment (DECINC)/DECINC.py
@@ -1,8 +1,21 @@
-n = int(input())
+n = input()
 
-if n%4 == 0:
-    n = n+1
-    print(n)
-else:
-    n = n-1
-    print(n)
+
+def dec_inc(num):
+    # Increment num by 1, if divisible by 4. Else decrement by 1.
+
+        if num%4 == 0:
+            num += 1
+            print(num)
+        else:
+            num -= 1
+            print(num)
+
+# Input Validation below, checking for int(n)
+try:
+    dec_inc(int(n))
+
+
+
+except: # Error message if int(n) == False
+    print('\nInvalid input - please enter an integer.') 
