e138ce2f386302cd298688e2024387646afa9fe1
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-09-30 23:15:20 +0530

    Update 1_Sum_of_elements.java

--- ALGORITHMS/Array/Java/1_Sum_of_elements.java
+++ ALGORITHMS/Array/Java/1_Sum_of_elements.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public static ArrayList<Integer> duplicates(int arr[], int n) {
+    public static ArrayList<Integer> sum(int arr[]) {
         // code here
     }
 }
