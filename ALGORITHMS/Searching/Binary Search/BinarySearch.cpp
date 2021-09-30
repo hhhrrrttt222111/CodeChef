05674dceef1b54becc15090aac9d359c04212fcb
NitikaGupta16 <58361725+NitikaGupta16@users.noreply.github.com>
2021-10-01 01:01:55 +0530

    Nqueens added

--- ALGORITHMS/Searching/Binary Search/BinarySearch.cpp
+++ ALGORITHMS/Searching/Binary Search/BinarySearch.cpp
@@ -1,25 +1,38 @@
-// C++ program to implement recursive Binary Search
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int binarySearch(int arr[], int l, int r, int x)
-{
-	if (r >= l) {
-		int mid = l + (r - l) / 2;
-		if (arr[mid] == x)
-			return mid;
-		if (arr[mid] > x)
-			return binarySearch(arr, l, mid - 1, x);
-		return binarySearch(arr, mid + 1, r, x);
-	}
-	return -1;
+
+int binarySearch(int arr[], int n, int key) {
+    int s = 0;
+    int e = n;
+    while (s <= e) 
+    {
+        int mid = (s + e)/2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }else if(arr[mid] > key) {
+            e = mid - 1;
+        }else {
+            s = mid + 1;
+        }
+    }
+    return -1;
 }
 
 int main()
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int result = binarySearch(arr, 0, n - 1, x);
-	(result == -1) ? cout << "Element is not present in array":cout << "Element is present at index " << result;
-	return 0;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key;
+    cin >> key; 
+
+    cout << binarySearch(arr, n, key) << endl;
+    return 0;
 }
