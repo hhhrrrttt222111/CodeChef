1c994ab430cef00facca35169d9bb6dbf610286f
Hritik Maheshwari <60053891+CRAZYGEEKS04@users.noreply.github.com>
2021-10-01 16:23:36 +0530

    Update adakng.cpp
    It is EASY to understand the solution for beginner because i am also a beginner.
    Please merge my pull request

--- Beginner/Ada King (ADAKNG)/adakng.cpp
+++ Beginner/Ada King (ADAKNG)/adakng.cpp
@@ -1,27 +1,56 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define jaadu ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-typedef unsigned long long int uli;
-int  chessBoard(int r, int c, int k){
-	int left = (c - k) < 1 ? 1 : (c-k);
-	int right = (c + k) > 8 ? 8 : (c + k);
-	int top = (r - k) < 1 ? 1 : (r - k);
-	int bottom = (r + k) >8 ? 8 : (r + k);
 
-	int length = right - left + 1;
-	int breadth = bottom - top + 1;
-
-	return length*breadth;
-}
-int main()
+int func(int t)
 {
-	jaadu;
+    if(t==0){
+        return 0;
+    }
+    int k;
+    char a[8][8];
+    cin>>k;
+    if(k>64){
+        return 0;
+    }
+    for(int j=0;j<8;j++){
+        for(int s=0;s<8;s++){
+            a[j][s]='X';
+        }
+    }
+    a[0][0]='O';
+    if(k>8)
+    {
+       for( int e=1;e<8;e++){
+          a[0][e]='.';
+        }
+        for(int r=0;r<k-8;r++){
+            a[1][r]='.';
+        }
+    } 
+    
+    else{
+        for(int i=1;i<k;i++)
+        {
+          a[0][i]='.';  
+        }
+    }
+    for(int d=0;d<8;d++){
+        for(int f=0;f<8;f++){
+           cout<<a[d][f];
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+    return func(--t);
+}
+
+int main() {
+	// your code goes here
 	int t;
 	cin>>t;
-	while(t--){
-		int r,c,k;
-		cin>>r>>c>>k;
-		cout<<chessBoard(r,c,k)<<endl;
+	if(t>64){
+	    return 0;
 	}
+	func(t);
 	return 0;
 }
