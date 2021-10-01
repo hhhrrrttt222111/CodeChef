898ee4cc76acf3585b227dca3980d820826d3121
Rishabh Arya <79662499+rishabh-arya@users.noreply.github.com>
2021-10-02 01:00:45 +0530

    Create Preorder_traversal.cpp

---
+++ ALGORITHMS/Tree/Tree Traversals/Preorder_traversal.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Defining the structure for nodes of the tree
+struct BinaryTreeNode
+{
+    int data;
+    struct BinaryTreeNode *left;
+    struct BinaryTreeNode *right;
+
+    BinaryTreeNode(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void PreorderTraversal(struct BinaryTreeNode *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    PreorderTraversal(root->left);
+    PreorderTraversal(root->right);
+}
+
+int main()
+{
+
+    struct BinaryTreeNode *root = new BinaryTreeNode(10);
+    root->left = new BinaryTreeNode(20);
+    root->right = new BinaryTreeNode(30);
+    root->left->left = new BinaryTreeNode(40);
+    root->left->right = new BinaryTreeNode(50);
+    root->right->left = new BinaryTreeNode(60);
+    root->right->right = new BinaryTreeNode(70);
+
+    /*
+-----------The formed tree will look like------
+                      
+                      10
+                    /    \
+                   20     30
+                  /  \    /  \
+                40   50  60   70
+*/
+
+    cout << "Preorder Traversal of the tree : ";
+    PreorderTraversal(root);
+
+    return 0;
+}
