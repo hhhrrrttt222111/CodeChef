c6c15d4b7b41d8ec9da4f7247e864fc4f8d72e4d
Sejal Koshta <86724161+sej28@users.noreply.github.com>
2021-10-02 17:12:19 +0530

    Create ForLoop.cpp
    Adding C++ solution to ForLoop.cpp problem from HackerRank
    
    https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem

---
+++ MORE CODING PLATFORMS/Hackerrank/For_Loop/ForLoop.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long n,m;
+    cin>>n;
+
+    cin>>m;
+    for(int i=n;i<=m;i++)
+    {
+        if(i<=9)
+        {
+            switch(i)
+            {
+                case 1:
+                {
+                cout<<"one\n";
+                break;
+                }
+                 case 2:
+                {
+                cout<<"two\n";
+                break;
+                }
+                 case 3:
+                {
+                cout<<"three\n";
+                break;
+                }
+                 case 4:
+                {
+                cout<<"four\n";
+                break;
+                }
+                 case 5:
+                {
+                cout<<"five\n";
+                break;
+                }
+                 case 6:
+                {
+                cout<<"six\n";
+                break;
+                }
+                 case 7:
+                {
+                cout<<"seven\n";
+                break;
+                }
+                 case 8:
+                {
+                cout<<"eight\n";
+                break;
+                }
+                 case 9:
+                {
+                cout<<"nine\n";
+                break;
+                }
+
+            }
+        }
+        else if(i>9)
+        {
+            if(i%2==0)
+            {
+                cout<<"even\n";
+            }
+            else {
+            {
+                cout<<"odd\n";
+            }
+            }
+        }
+    }
+    return 0;
+}
