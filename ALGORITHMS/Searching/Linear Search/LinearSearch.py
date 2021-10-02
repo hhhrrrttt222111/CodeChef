e5e9c39c74106a48b0195fcdd90b595f47860429
rajan-python1 <73902237+rajan-python1@users.noreply.github.com>
2021-10-02 18:14:01 +0530

    Create LinearSearch.py

---
+++ ALGORITHMS/Searching/Linear Search/LinearSearch.py
@@ -0,0 +1,20 @@
+
+def search(arr, n, x):
+
+	for i in range(0, n):
+		if (arr[i] == x):
+			return i
+	return -1
+
+
+
+arr = [2, 31, 4, 10, 45]
+x = 10
+n = len(arr)
+
+
+result = search(arr, n, x)
+if(result == -1):
+	print("Element is not present in array")
+else:
+	print("Element is present at index", result)
