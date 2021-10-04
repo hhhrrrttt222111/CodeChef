38c103269770f49351bea1b8b721607c5b1de92b
Danish Parveez <90828362+danish-p7@users.noreply.github.com>
2021-10-04 21:05:49 +0530

    Create october challenge 2021 div3-three boxes solution.cpp

---
+++ October challenge 2021 div 3 - THREE BOXES/october challenge 2021 div3-three boxes solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b, c, d, ans=0;
+        cin>>a>>b>>c>>d;
+        int sum=a+b+c;
+        if(sum<=d)ans=1;
+        else if((a==d && b==d && c==d) || (a+b>d && a+c>d && c+b>d))ans=3;
+        else ans=2;
+        
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
+
