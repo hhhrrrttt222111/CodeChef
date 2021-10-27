d77ba194376f4b1f88a70695b9e5bc802d675856
Anup Toggi <89697446+Anup-Toggi@users.noreply.github.com>
2021-10-27 19:38:33 +0530

    Deleted Olympics Ranking
    Deleted Olympics Ranking file

--- Beginner/Olympics Ranking.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for i in range(0,T):
-    ls = list(map(int, input().split()))
-    ls1 = ls[:len(ls)//2]
-    ls2 = ls[len(ls)//2:]
-    if(sum(ls1)>sum(ls2)):
-        print("1")
-    else:
-        print("2")
